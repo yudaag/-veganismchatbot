fce1c90440764983550f24340b2da1bbe3690e0a
yudaag <151615035+yudaag@users.noreply.github.com>
2025-06-03 13:10:32 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -29,7 +29,7 @@ def show():
 
     # 환경변수 로드
     load_dotenv()
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "teset-459015-9880036b726e.json"
 
     def get_image_base64(image_path):
         with open(image_path, "rb") as image_file:
