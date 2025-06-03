fda566a7552b4f709eb663e4cb71a04d967ee9dd
yudaag <151615035+yudaag@users.noreply.github.com>
2025-06-03 12:36:50 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import start
+import infoslide as infoslide
+import chatbot as chatbot
+import info as info
+
+def main():
+    if "page" not in st.session_state:
+        st.session_state.page = "start"
+
+    if st.session_state.page == "start":
+        start.show()
+    elif st.session_state.page == "infoslide":
+        infoslide.show()
+    elif st.session_state.page == "chatbot":
+        chatbot.show()
+    elif st.session_state.page == "info":
+        info.show()
+
+if __name__ == "__main__":
+    main()
